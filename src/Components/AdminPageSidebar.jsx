d359c534221a83400760e20536385ce07336e075
Replit user <>
2022-12-29 15:07:59 +0000

    modifications

--- src/Components/AdminPageSidebar.jsx
+++ src/Components/AdminPageSidebar.jsx
@@ -1,15 +1,38 @@
-import { Box, Flex, Text, Button } from "@chakra-ui/react"
-import { Link } from "react-router-dom"
+import { Box, VStack, Flex, Text, Button } from "@chakra-ui/react"
+import { Link } from "react-router-dom";
+// import { FaDownArrow } from "react-icons/fa"
 
 export default function AdminPageSidebar() {
 	return (
-		<Box bg="gray.200" h="100vh" w="25vw" >
-			<Flex>
-				<Link to="users">Users</Link>
-			</Flex>
-			<Flex>
-				<Link to="blogs">Blogs</Link>
-			</Flex>
-		</Box>
+		<VStack gap="2" p="4" bg="transparent" h="100vh" w="25vw" >
+			<Link style={{width:"100%"}} to="users">
+				<Flex 
+					w="full"
+					p="3"
+					rounded="md"
+					bg="gray.200"
+					justify="space-between" 
+					>
+					<Text fontWeight="600" color="gray.700">
+						Users
+					</Text>
+					>
+				</Flex>
+			</Link>
+			<Link style={{width:"100%"}} to="blogs">
+				<Flex 
+					w="full"
+					p="3"
+					rounded="md"
+					bg="gray.200"
+					justify="space-between" 
+					>
+					<Text fontWeight="600" color="gray.700">
+						Blogs
+					</Text>
+					>
+				</Flex>
+			</Link>
+		</VStack>
 	)
 }

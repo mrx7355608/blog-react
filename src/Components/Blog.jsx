fa662ecb632fe61a812071f68562684fbb72bf61
mrx7355608 <mrx7355608@gmail.com>
2022-12-31 15:52:21 +0500

    redesigned blog cards

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -3,38 +3,36 @@ import React from "react";
 
 export default function Blog() {
     return (
-        <Flex
-            bg="white"
-            mt="6"
-            border="1px"
-            borderColor="gray.200"
-            rounded="lg"
-            shadow="md"
-            direction="column"
-            p="5">
+        <Flex bg="transparent" mt="6" direction="column" p="5">
             <Text color="gray.500" fontWeight={600}>
                 11/02/2022
             </Text>
-            <Heading lineHeight={1} fontSize="2xl" fontFamily="Rajdhani" mt="2" fontWeight="700">
+            <Heading
+                color="red.500"
+                lineHeight={1}
+                fontSize="2xl"
+                fontFamily="Rajdhani"
+                mt="2"
+                fontWeight="700">
                 Mini Redux with Context API and useReducer
             </Heading>
             <Flex wrap="wrap" mt="2" gap="1">
                 <Tag
                     fontFamily="Rajdhani"
                     px="3"
-                    fontSize="xs"
                     borderRadius="full"
-                    variant={"outline"}
-                    colorScheme="red">
-                    <Text fontWeight={700}>Node</Text>
+                    variant="outline"
+                    color="gray.700"
+                    border="gray.700">
+                    <Text fontWeight={700}>Docker</Text>
                 </Tag>
                 <Tag
                     fontFamily="Rajdhani"
                     px="3"
-                    fontSize="xs"
                     borderRadius="full"
-                    variant={"outline"}
-                    colorScheme="red">
+                    variant="outline"
+                    color="gray.700"
+                    border="gray.700">
                     <Text fontWeight={700}>Expressjs</Text>
                 </Tag>
             </Flex>
@@ -42,15 +40,22 @@ export default function Blog() {
                 letterSpacing={0.5}
                 fontSize="16px"
                 lineHeight={1.6}
-                color="gray.600"
+                color="gray.700"
                 fontWeight={500}
                 mt="3.5">
                 Redux no doubt is a great tool for state management, but every tool cannot be used
                 everywhere, it might be an overkill if we used redux in a medium level project. But
                 we can't use state ....
             </Text>
-            <Button size="sm" mt="9" fontWeight={700} colorScheme="red" borderRadius="md">
-                Read
+            <Button
+                size="sm"
+                mt="3"
+                fontWeight={700}
+                colorScheme="blue"
+                variant="link"
+                w="max-content"
+                borderRadius="md">
+                Read more
             </Button>
         </Flex>
     );

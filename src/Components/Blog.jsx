bd11f61c5ae6a7d460ef348a0ebceef41025090c
mrx7355608 <mrx7355608@gmail.com>
2022-12-31 15:56:44 +0500

    changed blog's title color and its margin top

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -3,12 +3,12 @@ import React from "react";
 
 export default function Blog() {
     return (
-        <Flex bg="transparent" mt="6" direction="column" p="5">
+        <Flex bg="transparent" mt="9" direction="column" p="5">
             <Text color="gray.500" fontWeight={600}>
                 11/02/2022
             </Text>
             <Heading
-                color="red.500"
+                color="red.600"
                 lineHeight={1}
                 fontSize="2xl"
                 fontFamily="Rajdhani"

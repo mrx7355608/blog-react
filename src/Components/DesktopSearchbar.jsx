26f653f28a3d8c39a566006d6d3d68dc6665ecab
mrx7355608 <mrx7355608@gmail.com>
2023-01-06 02:33:00 +0500

    added searchbar on homepage

---
+++ src/Components/DesktopSearchbar.jsx
@@ -0,0 +1,32 @@
+import { Box, Input } from "@chakra-ui/react";
+import { FaSearch } from "react-icons/fa";
+
+export default function DesktopSearchbar() {
+    return (
+        <Box display={{ base: "none", lg: "block" }} pos="relative">
+            <Input
+                _placeholder={{
+                    opacity: 1,
+                    color: "gray.800"
+                }}
+                placeholder="Search"
+                fontWeight="600"
+                px="4"
+                pr="7"
+                size="lg"
+                variant="outline"
+                borderColor="gray.600"
+                color="gray.800"
+            />
+            <FaSearch
+                size="14px"
+                style={{
+                    position: "absolute",
+                    right: "3%",
+                    top: "50%",
+                    transform: "translateY(-50%)"
+                }}
+            />
+        </Box>
+    );
+}
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Container } from "@chakra-ui/react";
-import Blog from "../Components/Blog";
+
+import DesktopSearchbar from "../Components/DesktopSearchbar";
+import BlogCard from "../Components/BlogCard";
 
 export default function Home() {
     return (
         <Container maxW="700px" py="6">
-            <Blog />
-            <Blog />
-            <Blog />
-            <Blog />
-            <Blog />
+            <DesktopSearchbar />
+            <BlogCard />
+            <BlogCard />
+            <BlogCard />
+            <BlogCard />
+            <BlogCard />
         </Container>
     );
 }

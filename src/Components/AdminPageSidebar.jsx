44bef678f0fe9291930856503b4bfac249ec1593
mrx7355608 <mrx7355608@gmail.com>
2023-01-11 04:38:43 +0500

    added a link to admin dashboard home in sidebar

--- src/Components/AdminPageSidebar.jsx
+++ src/Components/AdminPageSidebar.jsx
@@ -1,38 +1,25 @@
-import { Box, VStack, Flex, Text, Button } from "@chakra-ui/react"
+import { Box, VStack, Flex, Text, Button } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 // import { FaDownArrow } from "react-icons/fa"
 
 export default function AdminPageSidebar() {
-	return (
-		<VStack gap="2" p="4" bg="transparent" h="100vh" w="25vw" >
-			<Link style={{width:"100%"}} to="users">
-				<Flex 
-					w="full"
-					p="3"
-					rounded="md"
-					bg="gray.200"
-					justify="space-between" 
-					>
-					<Text fontWeight="600" color="gray.700">
-						Users
-					</Text>
-					>
-				</Flex>
-			</Link>
-			<Link style={{width:"100%"}} to="blogs">
-				<Flex 
-					w="full"
-					p="3"
-					rounded="md"
-					bg="gray.200"
-					justify="space-between" 
-					>
-					<Text fontWeight="600" color="gray.700">
-						Blogs
-					</Text>
-					>
-				</Flex>
-			</Link>
-		</VStack>
-	)
+    return (
+        <VStack gap="2" p="4" bg="transparent" h="100vh" w="25vw">
+            <Link style={{ width: "100%" }} to="/admin">
+                <Flex w="full" p="3" rounded="md" bg="gray.700" justify="space-between">
+                    <Text fontWeight="600">Dashboard</Text>
+                </Flex>
+            </Link>
+            <Link style={{ width: "100%" }} to="users">
+                <Flex w="full" p="3" rounded="md" bg="gray.700" justify="space-between">
+                    <Text fontWeight="600">Users</Text>
+                </Flex>
+            </Link>
+            <Link style={{ width: "100%" }} to="blogs">
+                <Flex w="full" p="3" rounded="md" bg="gray.700" justify="space-between">
+                    <Text fontWeight="600">Blogs</Text>
+                </Flex>
+            </Link>
+        </VStack>
+    );
 }

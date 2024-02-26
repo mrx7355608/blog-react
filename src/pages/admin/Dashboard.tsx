439d2150f2227d8465982350869e951db8edefc5
mrx7355608 <mrx7355608@gmail.com>
2024-02-26 23:57:29 +0500

    added logout functionality

--- src/pages/admin/Dashboard.tsx
+++ src/pages/admin/Dashboard.tsx
@@ -1,27 +1,46 @@
-import { useEffect } from "react";
+import { Spinner } from "../../components/Spinner";
 import useAuthFetch from "../../hooks/useAuthFetch";
 import { BlogCard } from "../../components/BlogCard";
-
-export interface IBlog {
-    title: string;
-    content: string;
-    tags: string[];
-    is_published: boolean;
-    published_on: string;
-}
+import { IBlog } from "../../types/blog";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Dashboard = () => {
+    const [logoutLoading, setLogoutLoading] = useState(false);
+    const navTo = useNavigate();
     const { loading, error, response } = useAuthFetch<IBlog[]>(
         "http://localhost:8000/api/blogs",
     );
 
     return (
         <div className="flex items-start w-full flex-col gap-3 p-6">
-            <div className="flex justify-between items-center w-full">
+            <div className="flex justify-between items-center w-full mb-8">
                 <h1 className="text-4xl font-bold inline">Blogs</h1>
-                <button className="btn btn-error text-white">Logout</button>
+                <button onClick={logout} className="btn btn-error text-white">
+                    {logoutLoading ? <Spinner /> : "Logout"}
+                </button>
             </div>
-            <div></div>
+            {loading && <Spinner />}
+            {error && <p className="text-red-700 font-bold text-lg">{error}</p>}
+            {response &&
+                response.data.map((blog: IBlog) => {
+                    return <BlogCard blog={blog} />;
+                })}
         </div>
     );
+
+    async function logout() {
+        setLogoutLoading(true);
+        fetch("http://localhost:8000/api/auth/logout", {
+            method: "POST",
+            credentials: "include",
+        })
+            .then((resp) => {
+                if (resp.ok) {
+                    navTo("/admin/login");
+                }
+            })
+            .catch((err) => alert(err.message))
+            .finally(() => setLogoutLoading(false));
+    }
 };
---
+++ src/types/blog.ts
@@ -0,0 +1,8 @@
+export interface IBlog {
+    title: string;
+    content: string;
+    summary: string;
+    tags: string[];
+    is_published: boolean;
+    published_on: string;
+}

d5f467b8d4affba9f3bbe0e4580ebb3acde9189e
mrx7355608 <mrx7355608@gmail.com>
2024-03-10 19:37:23 +0500

    added roboto font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        fontFamily: {
+            roboto: ["Roboto", "sans-serif"],
+        },
+        daisyui: {
+            themes: ["light", "dark", "cupcake"],
+        },
     },
     plugins: [require("daisyui")],
 };

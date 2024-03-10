481ac51407c8eff56942dbdcdc66cc8a000fe182
mrx7355608 <mrx7355608@gmail.com>
2024-03-10 19:51:04 +0500

    populated publish and draft pages

--- src/pages/admin/DraftBlogs.tsx
+++ src/pages/admin/DraftBlogs.tsx
@@ -1,5 +1,21 @@
-import React from "react";
+import useAuthFetch from "../../hooks/useAuthFetch";
+import { IBlog } from "../../types/blog";
+import { Spinner } from "../../components/Spinner";
+import { BlogCard } from "../../components/BlogCard";
 
-export const DraftBlogs = (props: {}) => {
-    return <div>Draft blogs</div>;
+export const DraftBlogs = () => {
+    const { loading, error, response } = useAuthFetch<IBlog[]>(
+        "http://localhost:8000/api/blogs/un-published"
+    );
+
+    return (
+        <div className="flex items-start w-full flex-col gap-3 p-6">
+            {loading && <Spinner />}
+            {error && <p className="text-red-700 font-bold text-lg">{error}</p>}
+            {response &&
+                response.data.map((blog: IBlog) => {
+                    return <BlogCard blog={blog} />;
+                })}
+        </div>
+    );
 };
--- src/pages/admin/PublishedBlogs.tsx
+++ src/pages/admin/PublishedBlogs.tsx
@@ -1,5 +1,21 @@
-import React from "react";
+import useAuthFetch from "../../hooks/useAuthFetch";
+import { IBlog } from "../../types/blog";
+import { Spinner } from "../../components/Spinner";
+import { BlogCard } from "../../components/BlogCard";
 
-export const PublishedBlogs = (props: {}) => {
-  return <div>Published blogs</div>;
+export const PublishedBlogs = () => {
+    const { loading, error, response } = useAuthFetch<IBlog[]>(
+        "http://localhost:8000/api/blogs/published"
+    );
+
+    return (
+        <div className="flex items-start w-full flex-col gap-3 p-6">
+            {loading && <Spinner />}
+            {error && <p className="text-red-700 font-bold text-lg">{error}</p>}
+            {response &&
+                response.data.map((blog: IBlog) => {
+                    return <BlogCard blog={blog} />;
+                })}
+        </div>
+    );
 };

69a31e0dbb02b726063a3f7769d4bd27964f9585
mrx7355608 <mrx7355608@gmail.com>
2024-03-11 05:57:08 +0500

    adding top and bottom padding in homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,15 +3,14 @@ import { Spinner } from "../components/Spinner";
 import { IBlog } from "../types/blog";
 import useAuthFetch from "../hooks/useAuthFetch";
 import Searchbar from "../components/Searchbar";
-import { Link } from "react-router-dom";
 
 export const Home = () => {
     const { loading, error, response } = useAuthFetch<IBlog[]>(
         "http://localhost:8000/api/blogs/published"
     );
     return (
-        <div className="flex items-start gap-2 p-4">
-            <div className="flex items-center justify-start flex-col w-3/4 py-12 px-8">
+        <div className="flex items-start gap-2 py-12 px-8">
+            <div className="flex items-center justify-start flex-col w-3/4">
                 {loading && <Spinner />}
                 {error && (
                     <p className="text-red-700 font-bold text-lg">{error}</p>

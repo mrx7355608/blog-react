201506114dc22b2f0c224387b64baa0b374c4e16
mrx7355608 <mrx7355608@gmail.com>
2024-03-11 05:31:32 +0500

    created a searchbar

---
+++ src/components/Searchbar.tsx
@@ -0,0 +1,15 @@
+import { FaSearch } from "react-icons/fa";
+
+export default function Searchbar() {
+    return (
+        <form className="w-full relative">
+            <input type="text" className="input input-bordered w-full" />
+            <button
+                type="submit"
+                className="btn btn-md btn-ghost absolute top-0 right-0 text-gray-500"
+            >
+                <FaSearch size={14} />
+            </button>
+        </form>
+    );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,28 @@
+import { BlogCard } from "../components/BlogCard";
+import { Spinner } from "../components/Spinner";
+import { IBlog } from "../types/blog";
+import useAuthFetch from "../hooks/useAuthFetch";
+import Searchbar from "../components/Searchbar";
+
 export const Home = () => {
+    const { loading, error, response } = useAuthFetch<IBlog[]>(
+        "http://localhost:8000/api/blogs/published"
+    );
     return (
-        <>
-            <h1>Home</h1>
-        </>
+        <div className="flex items-start gap-2 p-4">
+            <div className="flex items-center justify-start flex-col w-3/4">
+                {loading && <Spinner />}
+                {error && (
+                    <p className="text-red-700 font-bold text-lg">{error}</p>
+                )}
+                {response &&
+                    response.data.map((blog: IBlog) => {
+                        return <BlogCard blog={blog} />;
+                    })}
+            </div>
+            <div className="w-1/4 flex items-center justify-start flex-col">
+                <Searchbar />
+            </div>
+        </div>
     );
 };

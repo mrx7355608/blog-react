2a32bef5cf580402b38c0781f04d3eda95fd3c8b
mrx7355608 <mrx7355608@gmail.com>
2024-03-14 00:54:19 +0500

    fixed url

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,10 @@ export const Home = () => {
     return (
         <div className="bg-black min-h-screen flex items-start justify-center py-12 px-12 w-full">
             <div className="flex items-center justify-start flex-col max-w-4xl">
-                <InfiniteScroll endpoint={"/api/blogs/published"} Card={BlogCard} />
+                <InfiniteScroll
+                    endpoint={"/api/blogs/published"}
+                    Card={BlogCard}
+                />
             </div>
         </div>
     );

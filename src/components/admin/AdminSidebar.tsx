2868cdf6b8eae2f924c041eefe5b11092c41f848
mrx7355608 <mrx7355608@gmail.com>
2024-03-17 18:25:52 +0500

    modified sidebar styles

--- src/components/admin/AdminSidebar.tsx
+++ src/components/admin/AdminSidebar.tsx
@@ -3,30 +3,26 @@ import { NavLink } from "react-router-dom";
 export const AdminSidebar = () => {
     const links = [
         {
-            icon: "/home.png",
             title: "Home",
             url: "/admin/dashboard",
         },
         {
-            icon: "/published.png",
             title: "Published blogs",
             url: "/admin/published",
         },
         {
-            icon: "/draft.png",
             title: "Draft Blogs",
             url: "/admin/drafts",
         },
         {
-            icon: "/create.png",
             title: "Create",
             url: "/admin/create-blog",
         },
     ];
 
-    const activeClasses = "bg-gray-200 cursor-pointer w-full p-4 rounded-lg";
+    const activeClasses = "bg-zinc-900 cursor-pointer w-full p-4 rounded-lg";
     const notActiveClass =
-        "bg-transparent cursor-pointer w-full p-4 rounded-lg hover:bg-gray-200";
+        "bg-transparent cursor-pointer w-full p-4 rounded-lg hover:bg-zinc-900";
 
     return (
         <div className="flex flex-col w-1/4 h-screen p-4 gap-2">
@@ -40,13 +36,7 @@ export const AdminSidebar = () => {
                         }}
                         to={link.url}
                     >
-                        <img
-                            src={link.icon}
-                            className="object-cover inline"
-                            width={25}
-                            height={25}
-                        />
-                        <span className="text-lg ml-2 font-medium">
+                        <span className="text-lg ml-2 font-medium text-white">
                             {link.title}
                         </span>
                     </NavLink>

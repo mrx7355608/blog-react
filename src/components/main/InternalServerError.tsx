e21088a16a3e1382b49730f85f071c6a36bec14c
mrx7355608 <mrx7355608@gmail.com>
2024-03-20 18:12:48 +0500

    removed un-necessary components

--- src/components/main/InternalServerError.tsx
+++ src/components/main/InternalServerError.tsx
@@ -1,9 +1,29 @@
+import { Link, isRouteErrorResponse, useRouteError } from "react-router-dom";
+
 export default function InternalServerError() {
+    const error = useRouteError();
+
+    if (isRouteErrorResponse(error) && error.status === 401) {
+        return (
+            <div className="w-full h-screen flex flex-col items-center justify-center ">
+                <h1 className="text-5xl font-bold">{error.data}</h1>
+                <p className="text-gray-600 text-2xl mt-5">
+                    Please login to continue
+                </p>
+                <Link to="/login">
+                    <button className="btn btn-info max-w-md mt-8">
+                        Login
+                    </button>
+                </Link>
+            </div>
+        );
+    }
+
     return (
         <div className="w-full h-screen flex flex-col items-center justify-center ">
             <h1 className="text-5xl font-bold">Something went wrong</h1>
-            <p className="text-gray-700 text-xl mt-4">
-                Please try again in few minutes
+            <p className="text-gray-500 text-2xl mt-5">
+                It seems that the server is down
             </p>
         </div>
     );
--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import { AdminSidebar } from "../components/admin/AdminSidebar";
 import { useEffect, useState, Suspense } from "react";
 import { useNavigate } from "react-router-dom";
@@ -9,13 +9,11 @@ import ToastProvider from "../context/toast";
 import { getAdmin } from "../services/admin.services";
 
 export const AdminLayout = () => {
-    const navTo = useNavigate();
-    const [loading, setLoading] = useState(true);
-    const [showInternalError, setShowInternalError] = useState(false);
+    const navigation = useNavigation();
 
     return (
         <div className="flex items-start justify-center w-full">
-            {navi}
+            {navigation.state === "loading" && <MainPagesSpinner />}
             <ToastProvider>
                 <AdminSidebar />
                 <div className="w-3/4 p-4">
